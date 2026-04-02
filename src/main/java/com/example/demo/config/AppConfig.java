2d6436c99f7ceb510a9af7629f67d9225b8f1b5c
DongGeun <ldg030609@yonsei.ac.kr>
2026-04-02 20:58:57 +0900

    [FIX] discount policy service : Auto DI (@ComponentScan ver)

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,21 +1,22 @@
 package com.example.demo.config;
 
-import com.example.demo.discount.FixDiscountPolicy;
-// import com.example.demo.discount.RateDiscountPolicy;
+// import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.service.OrderService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.example.demo")
 public class AppConfig {
-    
-    @Bean
-    public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();  // Here : select the type of discount policy
-    }
 
     @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();  // Here : select the type of discount policy
+    }
+
     public OrderService orderService() {
         return new OrderService(discountPolicy());
     }
