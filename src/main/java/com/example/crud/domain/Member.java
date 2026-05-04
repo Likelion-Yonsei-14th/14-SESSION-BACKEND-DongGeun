e6c72147bbd8fe3676809bb05530a065b39dccf2
DongGeun <ldg030609@yonsei.ac.kr>
2026-05-04 11:46:06 +0900

    [FEAT] MySQL 연동

--- src/main/java/com/example/crud/domain/Member.java
+++ src/main/java/com/example/crud/domain/Member.java
@@ -3,15 +3,21 @@ package com.example.crud.domain;
 import jakarta.persistence.*;
 
 @Entity // class가 DB table과 1:1로 매칭되도록 ㅏㅁ
-
+@Table(name = "members")
 public class Member {
     
     @Id // 이 필드가 Primary Key(PK)임을 뜻함
     @GeneratedValue(strategy = GenerationType.IDENTITY) // ID를 1, 2, 3... 자동으로 생성
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
 
     public Member() {}
 
@@ -24,4 +30,7 @@ public class Member {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    
+    public String getNickname() { return nickname; }
+    public void setNickname(String nickname) { this.nickname = nickname; }
 }
